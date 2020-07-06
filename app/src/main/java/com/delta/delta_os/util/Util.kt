633b4338c472602e245645c222d79e11e13fae26
maiquelknechtel <maiquelknechtel@maiquelknechtels-MacBook-Pro.local>
2020-07-06 19:23:12 -0300

    Commit para TESTE  das configurações do git

--- app/src/main/java/com/delta/delta_os/util/Util.kt
+++ app/src/main/java/com/delta/delta_os/util/Util.kt
@@ -1,7 +1,13 @@
 package com.delta.delta_os.util
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.*
+
 
 class Util {
     fun Date.toStringDate(format: String, locale: Locale = Locale.getDefault()): String {
@@ -12,9 +18,31 @@ class Util {
     public fun getCurrentDateTime(): Date {
         return Calendar.getInstance().time
     }
-    fun toSimpleString(date: Date) : String {
+
+    fun toSimpleString(date: Date): String {
         val format = SimpleDateFormat("dd/MM/yyy")
         return format.format(date)
+    }
+
+    //@RequiresApi(Build.VERSION_CODES.O)
+    fun toDate(string: String): Date? {
+
+        if (string.equals("dataSaida")) {
+            val date2 = SimpleDateFormat("dd/MM/yyyy")
+            return  null;
+        }
+        if (string.equals("")) {
+            val date2 = SimpleDateFormat("dd/MM/yyyy")
+            return  null;
+        }
+        if (string.equals("dataEntrada")){
+        //    return Date()
+            val date2 = SimpleDateFormat("dd/MM/yyyy").parse("01/01/2020")
+            return  null;
+        }
+        val date2 = SimpleDateFormat("dd/MM/yyyy").parse(string)
+
+        return date2;
     }
 
 
