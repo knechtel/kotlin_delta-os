42a0e3858d124b29953e4ead7531cd151cf250a2
knechtel <maiquelknechtel@gmail.com>
2022-01-15 19:52:47 -0300

    Adiciionando classe de orden de serviço.

--- app/src/main/java/com/delta/delta_os/bean/Os.kt
+++ app/src/main/java/com/delta/delta_os/bean/Os.kt
@@ -1,6 +1,13 @@
 package com.delta.delta_os.bean
 
+import java.util.*
+
 class Os {
+
+    var dataEntrada:Date?=null
+    var dataSaida:Date?=null
+    var dataEntradaGarantia:Date?=null
+    var dataSaidaGarantia:Date?=null
     var client:Cliente?=null
     var aparelho: Aparelho?
         get() = null
